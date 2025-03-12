e2e7a6f44e3491cb0fa653ffb99bf79a5f4bb2e3
ywatanabe <yu.w.tennis@gmail.com>
2025-03-12 17:46:29 +0900

    feat: Added views based on threshold

---
+++ sql/bigquery/V1.3.0__remove_unwanted_chars_from_view_name.sql
@@ -0,0 +1,35 @@
+DROP VIEW IF EXISTS dsongcp.flights;
+
+-- VIEW reflecting ALL records
+CREATE OR REPLACE VIEW dsongcp.flights AS
+SELECT
+    FlightDate AS FL_DATE,
+    Reporting_Airline AS UNIQUE_CARRIER,
+    OriginAirportSeqID AS ORIGIN_AIRPORT_SEQ_ID,
+    Origin AS ORIGIN,
+    DestAirportSeqID AS DEST_AIRPORT_SEQ_ID,
+    Dest AS DEST,
+    CRSDepTime AS CRS_DEP_TIME,
+    DepTime AS DEP_TIME,
+    CAST(DepDelay AS FLOAT64) AS DEP_DELAY,
+    CAST(TaxiOut AS FLOAT64) AS TAXI_OUT,
+    WheelsOff AS WHEELS_OFF,
+    WheelsOn AS WHEELS_ON,
+    CAST(TaxiIn AS FLOAT64) AS TAXI_IN,
+    CRSArrTime AS CRS_ARR_TIME,
+    ArrTime AS ARR_TIME,
+    CAST(ArrDelay AS FLOAT64) AS ARR_DELAY,
+    IF(Cancelled = '1.00', True, False) AS CANCELLED,
+    IF(Diverted = '1.00', True, False) AS DIVERTED,
+    DISTANCE
+FROM dsongcp.flights_raw;
+
+-- Threshold based VIEWs
+CREATE OR REPLACE VIEW dsongcp.delayed_10_2015 AS
+SELECT * FROM dsongcp.flights WHERE dep_delay >= 10;
+
+CREATE OR REPLACE VIEW dsongcp.delayed_15_2015 AS
+SELECT * FROM dsongcp.flights WHERE dep_delay >= 15;
+
+CREATE OR REPLACE VIEW dsongcp.delayed_20_2015 AS
+SELECT * FROM dsongcp.flights WHERE dep_delay >= 20;
